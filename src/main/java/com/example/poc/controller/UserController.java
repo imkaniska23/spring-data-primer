285e71234a18968dae0a758db23f12312131cbf2
Kumar Kaniska <imkaniska237@gmail.com>
2023-06-02 17:43:16 +0530

    Added User entity, controller and repository

---
+++ src/main/java/com/example/poc/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.example.poc.controller;
+
+import com.example.poc.entity.User;
+import com.example.poc.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.ws.rs.Consumes;
+import java.util.List;
+
+@RestController
+@RequestMapping(path="/userController/v1")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping(path="/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        if(users.size()==0)
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        else
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(users);
+    }
+
+    @PostMapping(path="/user")
+    @Consumes(MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
---
+++ src/main/java/com/example/poc/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.poc.repository;
+
+import com.example.poc.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
